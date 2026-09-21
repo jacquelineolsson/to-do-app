3c0b9c4d38f707747795d14089cf99b4ca0b8bcc
Jacqueline <jacqueline.olsson@lerniastudent.se>
2026-09-21 15:37:47 +0200

    knapp och inpuit-en början

--- script.js
+++ script.js
@@ -1,16 +1,31 @@
 
 
-const addTodoBtn = documnt.querySelector("button");
+// declare variables
+const addTodoBtn = document.querySelector("button");
+const list = document.querySelector("ul");
+const input = document.querySelector("#todo-input");
+const infoText = document.querySelector("#infoText");
+
 
 addTodoBtn.addEventListener(
-
-
+    "click",
+    addTodoItem
 );
 
+
+// function to add a item to the list
 function addTodoItem(){
 
-"click",
-addTodoItem
+infoText.textContent = "";
+const text = input.value;
+
+
+const listItem = document.createElement("li");
+list.appendChild(listItem);
+
+const itemlabel = document.createElement("li");
+itemlabel.textContent = text;
+listItem.appendChild(itemlabel);
 
 }
 
